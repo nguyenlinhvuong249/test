52bda4f2d32e73360c17fc549600be64b9af2c9e
LinhVuong1108 <167665584+LinhVuong1108@users.noreply.github.com>
2024-12-23 20:27:43 +0700

    Update code.ps1

--- code.ps1
+++ code.ps1
@@ -1,9 +1,12 @@
-# URL của file .bat trên GitHub
+# URL của file trên GitHub
 $FileUrl = "https://raw.githubusercontent.com/nguyenlinhvuong249/tools_setup_idata_share/refs/heads/main/idata%20setup%20share.bat"
 
-# Tải nội dung file và chuyển đến cmd.exe để thực thi
-Write-Host "Đang tải và thực thi file .bat từ URL..." -ForegroundColor Yellow
+# Tải và thực thi nội dung file từ URL
+Write-Host "Đang tải và thực thi file từ GitHub..." -ForegroundColor Yellow
 Invoke-WebRequest -Uri $FileUrl -UseBasicParsing | Select-Object -ExpandProperty Content | cmd.exe /c
 
-# Đóng PowerShell ngay lập tức sau khi chuyển lệnh đến cmd
+Write-Host "Hoàn tất thực thi." -ForegroundColor Green
+
+# Đóng cửa sổ PowerShell
+Write-Host "Đóng cửa sổ PowerShell..." -ForegroundColor Yellow
 exit
