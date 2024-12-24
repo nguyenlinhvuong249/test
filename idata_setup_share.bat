486206e7c6d48aab001cbac165d366c760692bf4
Nguyễn Linh Vương <162272562+nguyenlinhvuong249@users.noreply.github.com>
2024-12-24 11:02:57 +0700

    Delete idata_setup_share.bat

--- idata_setup_share.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Gán đường dẫn APK vào biến duong_dan_app_moi
-set "duong_dan_app_moi=%1"
-
-:: Kiểm tra giá trị của biến
-echo Đường dẫn file APK: %duong_dan_app_moi%
-
-:: Thực hiện các hành động khác với biến duong_dan_app_moi
-pause
